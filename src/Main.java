769e1098417483879c1fd5893b6a548014916e9f
Angle Qian <angleqian01@gmail.com>
2022-10-11 09:19:42 -0400

    bad snap to square

---
+++ src/Main.java
@@ -0,0 +1,253 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.w3c.dom.css.Rect;
+import processing.core.PApplet;
+
+
+public class Main extends PApplet
+{
+    //when in doubt, consult the Processsing reference: https://processing.org/reference/
+
+    int margin = 200; //set the margin around the squares
+    final int padding = 50; // padding between buttons and also their width/height
+    final int buttonSize = 40; // padding between buttons and also their width/height
+    ArrayList<Integer> trials = new ArrayList<Integer>(); //contains the order of buttons that activate in the test
+    int trialNum = 0; //the current trial number (indexes into trials array above)
+    int startTime = 0; // time starts when the first click is captured
+    int finishTime = 0; //records the time of the final click
+    int hits = 0; //number of successful clicks
+    int misses = 0; //number of missed clicks
+    Robot robot; //initialized in setup
+
+    int numRepeats = 1; //sets the number of times each button repeats in the test
+
+    // Our own variables
+
+    // 1) when justSnapped is false, then that means cursor can become snapped,
+    // so once cursor is "near" a button, it will snap to the center of the button,
+    // then justSnapped will be set to true
+    // 2) when justSnapped is true, then that means cursor cannot become snapped,
+    // the cursor must be far enough away from the last snapped button in order
+    // to have justSnapped be set of false again
+    // without this, cursor will never be able to escape after snapping
+    boolean justSnapped = false;
+
+    // button ID of the last button that was snapped to
+    int lastSnappedButtonID = -1;
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    @Override
+    public void settings() {
+        size(700,700); // set the size of the window
+    }
+
+    public void setup()
+    {
+        //noCursor(); // hides the system cursor if you want
+        noStroke(); //turn off all strokes, we're just using fills here (can change this if you want)
+        textFont(createFont("Arial",16)); //sets the font to Arial size 16
+        textAlign(CENTER);
+        frameRate(60); //normally you can't go much higher than 60 FPS.
+        ellipseMode(CENTER); //ellipses are drawn from the center (BUT RECTANGLES ARE NOT!)
+        //rectMode(CENTER); //enabling will break the scaffold code, but you might find it easier to work with centered rects
+
+        try {
+            robot = new Robot(); //create a "Java Robot" class that can move the system cursor
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        //===DON'T MODIFY MY RANDOM ORDERING CODE==
+        for (int i = 0; i < 16; i++) //generate list of targets and randomize the order
+            // number of buttons in 4x4 grid
+            for (int k = 0; k < numRepeats; k++)
+                // number of times each button repeats
+                trials.add(i);
+
+        Collections.shuffle(trials); // randomize the order of the buttons
+        System.out.println("trial order: " + trials); //print out order for reference
+
+        surface.setLocation(0,0);// put window in top left corner of screen (doesn't always work)
+    }
+
+
+    public void draw()
+    {
+        background(60); //set background to black
+
+        if (trialNum >= trials.size()) //check to see if test is over
+        {
+            float timeTaken = (finishTime-startTime) / 1000f;
+            float penalty = constrain(((95f-((float)hits*100f/(float)(hits+misses)))*.2f),0,100);
+            fill(255); //set fill color to white
+            //write to screen (not console)
+            text("Finished!", width / 2, height / 2);
+            text("Hits: " + hits, width / 2, height / 2 + 20);
+            text("Misses: " + misses, width / 2, height / 2 + 40);
+            text("Accuracy: " + (float)hits*100f/(float)(hits+misses) +"%", width / 2, height / 2 + 60);
+            text("Total time taken: " + timeTaken + " sec", width / 2, height / 2 + 80);
+            text("Average time for each button: " + nf((timeTaken)/(float)(hits+misses),0,3) + " sec", width / 2, height / 2 + 100);
+            text("Average time for each button + penalty: " + nf(((timeTaken)/(float)(hits+misses) + penalty),0,3) + " sec", width / 2, height / 2 + 140);
+            return; //return, nothing else to do now test is over
+        }
+
+        fill(255); //set fill color to white
+        text((trialNum + 1) + " of " + trials.size(), 40, 20); //display what trial the user is on
+
+        for (int i = 0; i < 16; i++)// for all button
+            drawButton(i); //draw button
+
+        fill(255, 0, 0, 200); // set fill color to translucent red
+        ellipse(mouseX, mouseY, 20, 20); //draw user cursor as a circle with a diameter of 20
+
+    }
+
+    public void mousePressed() // test to see if hit was in target!
+    {
+        if (trialNum >= trials.size()) //check if task is done
+            return;
+
+        if (trialNum == 0) //check if first click, if so, record start time
+            startTime = millis();
+
+        if (trialNum == trials.size() - 1) //check if final click
+        {
+            finishTime = millis();
+            //write to terminal some output:
+            System.out.println("we're all done!");
+        }
+
+        Rectangle bounds = getButtonLocation(trials.get(trialNum));
+
+        //check to see if cursor was inside button
+        if ((mouseX > bounds.x && mouseX < bounds.x + bounds.width) && (mouseY > bounds.y && mouseY < bounds.y + bounds.height)) // test to see if hit was within bounds
+        {
+            System.out.println("HIT! " + trialNum + " " + (millis() - startTime)); // success
+            hits++;
+        } else
+        {
+            System.out.println("MISSED! " + trialNum + " " + (millis() - startTime)); // fail
+            misses++;
+        }
+
+        trialNum++; // Increment trial number
+
+        //in this example design, I move the cursor back to the middle after each click
+//        robot.mouseMove(width/2, (height)/2); //on click, move cursor to roughly center of window!
+    }
+
+    //probably shouldn't have to edit this method
+    public Rectangle getButtonLocation(int i) //for a given button ID, what is its location and size
+    {
+        int x = (i % 4) * (padding + buttonSize) + margin;
+        int y = (i / 4) * (padding + buttonSize) + margin;
+
+        return new Rectangle(x, y, buttonSize, buttonSize);
+    }
+
+    //you can edit this method to change how buttons appear
+    public void drawButton(int i)
+    {
+        Rectangle bounds = getButtonLocation(i);
+
+        if (trials.get(trialNum) == i) // see if current button is the target
+            fill(0, 255, 255); // if so, fill cyan
+        else
+            fill(200); // if not, fill gray
+
+        rect(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+
+    public void mouseMoved()
+    {
+        //can do stuff everytime the mouse is moved (i.e., not clicked)
+        //https://processing.org/reference/mouseMoved_.html
+        Point currCursorPoint = new Point(mouseX, mouseY);
+//        System.out.println("Mouse location " + currCursorPoint.toString());
+        int closestButtonID = getClosestButtonID(currCursorPoint);
+
+        if (justSnapped && lastSnappedButtonID != -1) {
+            Rectangle lastSnappedButtonPaddingBox = getButtonPaddingBox(lastSnappedButtonID);
+            boolean isCursorOutsidePaddingBox = !isPointWithinRect(currCursorPoint, lastSnappedButtonPaddingBox);
+            if (isCursorOutsidePaddingBox) {
+                justSnapped = false;
+                System.out.println("Just unsnapped!");
+            }
+        }
+
+        if (!justSnapped) {
+            // the cursor can snap to something
+            Rectangle closestButtonPaddingBox = getButtonPaddingBox(closestButtonID);
+            boolean isCursorWithinPaddingBox = isPointWithinRect(currCursorPoint, closestButtonPaddingBox);
+            if (isCursorWithinPaddingBox) {
+                Point buttonCenter = getButtonCenter(closestButtonID);
+                robot.mouseMove(buttonCenter.x, buttonCenter.y);
+                justSnapped = true;
+                lastSnappedButtonID = closestButtonID;
+                System.out.println("Just snapped to " + str(lastSnappedButtonID));
+                System.out.println("Location snapped to is " + buttonCenter.toString());
+            }
+        }
+    }
+
+    public void mouseDragged()
+    {
+        //can do stuff everytime the mouse is dragged
+        //https://processing.org/reference/mouseDragged_.html
+    }
+
+    public void keyPressed()
+    {
+        //can use the keyboard if you wish
+        //https://processing.org/reference/keyTyped_.html
+        //https://processing.org/reference/keyCode.html
+        if (key == '0') {
+                justSnapped = false;
+                System.out.println("Unsnapped via keyboard");
+        }
+    }
+
+    private Point getButtonCenter(int i) // for a given button ID, what is its center
+    {
+        Rectangle rect = getButtonLocation(i);
+        return new Point(rect.x + buttonSize / 2, rect.y + 2 * buttonSize);
+    }
+
+    private Rectangle getButtonPaddingBox(int i) // for a given button ID, what is its padding box
+            // padding box refers to the box plus a ring of padding around it
+    {
+        Rectangle rect = getButtonLocation(i);
+        return new Rectangle(
+                rect.x - padding / 2,
+                rect.y - padding / 2,
+                rect.width + padding,
+                rect.height + padding);
+    }
+
+    private int getClosestButtonID(Point mouseLocation) // find the ID of the button that is closest to the current mouse
+    {
+        int buttonID = 0;
+        float minDist = 10000;
+        for (int i = 0; i < 16; ++i) {
+            float dist =  computeDist(mouseLocation, getButtonCenter(i));
+            if (dist < minDist) {
+                minDist = dist;
+                buttonID = i;
+            }
+        }
+        return buttonID;
+    }
+
+    private float computeDist(Point a, Point b) {
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    }
+
+    private boolean isPointWithinRect(Point p, Rectangle r) {
+        return (r.x <= p.x && p.x <= r.x + r.width && r.y <= p.y && p.y <= r.y + r.height);
+    }
+}
